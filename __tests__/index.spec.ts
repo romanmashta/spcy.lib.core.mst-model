807e41f45aa30dea38fc307c03fba55d0a53de33
Roman Mashta <roman.mashta@gmail.com>
2020-06-13 18:50:10 +0300

    Packages update

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,7 +1,27 @@
+import { getSnapshot, Instance, types } from 'mobx-state-tree';
+
 import { sayHi, sayBye } from '../src';
 
+const BasicTypes = types.union(types.literal('boolean'), types.literal('number'), types.literal('string'));
+
+const TypeInfo = types.union(BasicTypes);
+
+const TypeLiteral = types.model('TypeLiteral', {
+  properties: types.map(TypeInfo),
+});
+
 test('It says Hi', () => {
-  expect(sayHi('Me')).toBe('Hi Me');
+  const i = {
+    properties: {
+      firstName: 'string',
+      lastName: 'string',
+      age: 'number',
+      isActive: 'boolean',
+    },
+  };
+
+  const x = TypeLiteral.create(i);
+  console.log(getSnapshot(x));
 });
 
 test('It says Bye', () => {
