3b0c11d755fc53487268f549b34ec04ac23c8471
Roman Mashta <roman.mashta@gmail.com>
2020-06-14 03:40:31 +0300

    MST builder refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
 import _ from 'lodash';
 import { IAnyType, types } from 'mobx-state-tree';
-import {
-  ObjectType,
-  TypeInfo,
-  SimpleType,
-  ArrayType,
-  Module,
-  OneOf,
-  TypeReference,
-  isObjectType,
-  isSimpleType,
-  isArrayType,
-  isOneOf,
-  isTypeReference
-} from '@spcy/lib.core.reflection';
+import * as cr from '@spcy/lib.core.reflection';
 
 const simpleMap = {
   string: types.string,
@@ -23,21 +10,29 @@ const simpleMap = {
   null: types.null
 };
 
-export const buildModel = (def: ObjectType, name: string | undefined = undefined): IAnyType =>
-  types.model(name || 'Object', _.mapValues(def.properties, buildType));
-export const buildSimpleType = (def: SimpleType): IAnyType => simpleMap[def.type];
-export const buildArray = (def: ArrayType): IAnyType => types.array(buildType(def.items));
-export const buildOneOf = (def: OneOf): IAnyType => types.union(..._.map(def.oneOf, t => buildType(t)));
-export const buildTypeReference = (def: TypeReference): IAnyType => types.late(() => types.literal(def.$ref));
+class ModelBuilder {
+  buildModel = (def: cr.ObjectType, name: string | undefined = undefined): IAnyType =>
+    types.model(name || 'Object', _.mapValues(def.properties, this.buildType));
 
-export const buildType = (def: TypeInfo, name: string | undefined = undefined): IAnyType => {
-  if (isObjectType(def)) return buildModel(def, name);
-  if (isSimpleType(def)) return buildSimpleType(def);
-  if (isArrayType(def)) return buildArray(def);
-  if (isOneOf(def)) return buildOneOf(def);
-  if (isTypeReference(def)) return buildTypeReference(def);
-  return types.null;
-};
+  buildSimpleType = (def: cr.SimpleType): IAnyType => simpleMap[def.type];
 
-export const buildModule = (module: Module): { [name: string]: IAnyType } =>
-  _.mapValues(module.$defs, (def, name) => buildType(def, name));
+  buildArray = (def: cr.ArrayType): IAnyType => types.array(this.buildType(def.items));
+
+  buildOneOf = (def: cr.OneOf): IAnyType => types.union(..._.map(def.oneOf, t => this.buildType(t)));
+
+  buildTypeReference = (def: cr.TypeReference): IAnyType => types.late(() => types.literal(def.$ref));
+
+  buildType = (def: cr.TypeInfo, name: string | undefined = undefined): IAnyType => {
+    if (cr.isObjectType(def)) return this.buildModel(def, name);
+    if (cr.isSimpleType(def)) return this.buildSimpleType(def);
+    if (cr.isArrayType(def)) return this.buildArray(def);
+    if (cr.isOneOf(def)) return this.buildOneOf(def);
+    if (cr.isTypeReference(def)) return this.buildTypeReference(def);
+    return types.null;
+  };
+
+  buildModule = (module: cr.Module): { [name: string]: IAnyType } =>
+    _.mapValues(module.$defs, (def, name) => this.buildType(def, name));
+}
+
+export const buildModule = (module: cr.Module): { [name: string]: IAnyType } => new ModelBuilder().buildModule(module);
