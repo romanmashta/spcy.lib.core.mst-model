d83860ccec4932f6a7d8b9606e64da754139a3db
Roman Mashta <roman.mashta@gmail.com>
2020-06-22 03:49:40 +0300

    Resolving fixes

--- src/model-repository.ts
+++ src/model-repository.ts
@@ -1,25 +1,27 @@
-import { IAnyType, types } from '@spcy/pub.mobx-state-tree';
+import { IAnyType } from '@spcy/pub.mobx-state-tree';
 import { SchemaRepository } from '@spcy/lib.core.reflection';
 import { ModelBuilder } from './model-builder';
 import { ModelResolver } from './model-resolver';
 
 class ModelRepositoryInternal implements ModelResolver {
   private builder: ModelBuilder = new ModelBuilder(this);
-  private repo: Map<string, IAnyType> = new Map<string, IAnyType>();
+  private repo: Map<string, IAnyType | null> = new Map<string, IAnyType | null>();
 
   buildModel = (ref: string): IAnyType => {
     const schema = SchemaRepository.resolve(ref);
     if (!schema) {
       throw new Error(`Cannot resolve schema for ${ref}`);
     }
-    this.repo.set(ref, types.model(ref));
+    this.repo.set(ref, null);
     const model = this.builder.buildType(schema);
     this.repo.set(ref, model);
     return model;
   };
 
   resolve = (ref: string): IAnyType => {
-    return this.repo.get(ref) || this.buildModel(ref);
+    const model = this.repo.get(ref);
+    if (model === null) throw new ReferenceError();
+    return model || this.buildModel(ref);
   };
 }
 
